3cbd6463b5c45e2c136d7dcf56c61da36cff86f3
Areca <102679138+Arecary@users.noreply.github.com>
2023-01-14 15:25:52 -0500

    add the 'about me' button

--- app/src/main/java/edu/northeastern/numad23sp_shizeng/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_shizeng/MainActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad23sp_shizeng;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.button_AboutMe);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Shi Zeng\n"+"zeng.shi1@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
